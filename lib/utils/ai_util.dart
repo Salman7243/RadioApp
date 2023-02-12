487029a67e4b3c2d5c81b755ffb7256835812a7a
Salman7243 <98895288+Salman7243@users.noreply.github.com>
2023-02-12 18:32:00 +0400

    Add files via upload
    today

---
+++ lib/utils/ai_util.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+mixin AIColors {
+  static Color primaryColor1 = Vx.orange400;
+  static Color primaryColor2 = Vx.purple500;
+}
+
+class Vx {
+  static Color purple500;
+  
+  static Color orange400;
+}
